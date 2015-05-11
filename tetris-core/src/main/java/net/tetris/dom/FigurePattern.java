81624a866cf1522634cfd47c837c67c8f06f37e3
slav9nin <slav9nin2009@gmail.com>
2015-05-11 21:19:35 +0300

    added assistent and get method

--- tetris-core/src/main/java/net/tetris/dom/FigurePattern.java
+++ tetris-core/src/main/java/net/tetris/dom/FigurePattern.java
@@ -7,7 +7,9 @@ public class FigurePattern {
     private int centerY;
     private String present = "Present";
     private int nullzero;
+	private String hello = "Hello";
 	private int scores = 0;
+	private String assistent = "Alex";
 
     public FigurePattern(int centerX, int centerY, String... rows) {
         this.rows = rows;
@@ -33,5 +35,9 @@ public class FigurePattern {
 	
 	public String getHelloMessage() {
 		return hello;
+	}
+	
+	public String  getAssistent() {
+		return assistent;
 	}
 }
