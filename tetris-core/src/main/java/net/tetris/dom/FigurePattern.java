df0c13c43144a124cf30d2533cfa502924f54495
slav9nin <slav9nin2009@gmail.com>
2015-05-11 19:20:26 +0300

    Added field scores type int

--- tetris-core/src/main/java/net/tetris/dom/FigurePattern.java
+++ tetris-core/src/main/java/net/tetris/dom/FigurePattern.java
@@ -8,6 +8,7 @@ public class FigurePattern {
     private String present = "Present";
     private int nullzero;
 	private String hello = "Hello";
+	public int scores = 0;
 
     public FigurePattern(int centerX, int centerY, String... rows) {
         this.rows = rows;
